327aea78829b21fc6957c8515d646a00ba5be2c4
Vishal Tambi <tambivishal3@gmail.com>
2025-12-03 18:00:09 +0530

    added backend url after cors

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://react-to-do-app-cvuf.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
